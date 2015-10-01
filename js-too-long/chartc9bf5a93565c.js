a0baf0a4c00dc8179a303385f2a725c9276c6f89
Rob L Davidson <bobbledavidson@yahoo.co.uk>
2015-10-02 00:56:29 +0800

    removed x,y dimensions
    Use of window.innerHeight and window.innerWidth for x,y dimensions here was breaking responsiveness. 
    Now width is responsive and height is not. 
    Height can be made resizable by setting the rchart div (see 'render to' object here) to have the CSS properties:
    height: -moz-calc(100% - 50px);height: -webkit-calc(100% - 50px);height: calc(100% - 50px);position:absolute
    
    Where -50px is included in my case to allow the chart to fit the screen despite having addthis buttons above it. 
    This 'position:absolute' stipulation does screw up if you have anything underneath the chart, which will now float to the top of the page and be seen underneath your chart. Javascript is the only decent way to resize the chart - I've opted simply for fixed 400px height for my solution

--- js-too-long/chartc9bf5a93565c.js
+++ js-too-long/chartc9bf5a93565c.js
@@ -3,8 +3,6 @@
             var chart = new Highcharts.Chart({
                 "chart":{
                     "type":"scatter",
-                    "width":  window.innerWidth,
-                    "height": window.innerHeight-40,
                      "zoomType": "xy",
                      "renderTo": "chartc9bf5a93565c",
                     
